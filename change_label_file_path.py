37bf29d4643d151468ab4de3afc335b406303866
Mark Smertenko <125746052+markidzo@users.noreply.github.com>
2026-02-09 21:43:39 +0200

    Refactor change_label_file_path.py for clarity
    Updated comments and formatting in the script.

--- change_label_file_path.py
+++ change_label_file_path.py
@@ -4,18 +4,19 @@ import json
 input_json_path = "label_audio.json"   # вихідний JSON
 output_json_path = "label_change_result.json" # куди збережемо результат
 
-# Зчитуємо JSON
+# JSON
 with open(input_json_path, "r", encoding="utf-8") as f:
     data = json.load(f)
 
-# Оновлюємо шляхи
+# Оновгення шляхів
 for item in data.get("data", []):
     original_path = item.get("path", "")
-    # Замінюємо "/data" на потрібний шлях
+    #  "/data" на потрібний шлях
     item["path"] = original_path.replace("/data", "C:/labelstudio/data/media")
 
-# Записуємо результат у новий JSON
+# Зберіг. результат у новий JSON
 with open(output_json_path, "w", encoding="utf-8") as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
 
 print(f"Шляхи успішно оновлено і збережено у {output_json_path}")
+
